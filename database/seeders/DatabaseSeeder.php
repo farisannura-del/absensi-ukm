d4a7045508f86552f7a1f20308fb5d74a8188111
farisannura-del <farisannura28@gmail.com>
2026-07-26 04:29:47 +0700

    Create default admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,28 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            [
+                'email' => 'admin@ukmseni.com',
+            ],
+            [
+                'name' => 'Administrator',
+                'nim' => 'ADMIN001',
+                'bidang' => 'Kesekretariatan',
+                'google_id' => null,
+                'avatar' => null,
+                'role' => 'admin',
+                'password' => 'admin123',
+            ]
+        );
     }
 }
